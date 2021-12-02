699a8f2cada35ffa226ec68f3abc2ef6a8ad1287
Jered <mcinerney.de@northeastern.edu>
2021-12-02 12:44:23 -0500

    fixed configs

--- submit_job_val.sh
+++ submit_job_val.sh
@@ -2,4 +2,4 @@ module load anaconda3/3.7
 module load cuda/10.2
 module load discovery/2019-02-21
 source activate gloria_new
-python run.py -c configs/chexpert_pretrain_imagenome_val_config.yaml --val
+python run.py -c configs/imagenome_pretrain_val_config.yaml --val
